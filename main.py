627baf3d679faf0dc853ae172b8f484c1d334659
vaibhav <vaibhavg152@gmail.com>
2019-10-23 21:53:29 +0530

    translate

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import os
 from objLoader import *
 
 #from utils.kp_tools import kp_list_2_opencv_kp_list
-N = 25
-MIN_MATCHES = 150
+N = 100
+MIN_MATCHES = 140
 
 def projection_matrix(camera_parameters, homography):
 
@@ -36,9 +36,6 @@ def projection_matrix(camera_parameters, homography):
     return np.dot(camera_parameters, projection)
 
 def render(img, obj, projection, model, color=False):
-    """
-    Render a loaded obj model into the current video frame
-    """
     vertices = obj.vertices
     scale_matrix = np.eye(3) * 3
     h, w = model.shape
@@ -47,17 +44,10 @@ def render(img, obj, projection, model, color=False):
         face_vertices = face[0]
         points = np.array([vertices[vertex - 1] for vertex in face_vertices])
         points = np.dot(points, scale_matrix)
-        # render model in the middle of the reference surface. To do so,
-        # model points must be displaced
         points = np.array([[p[0] + w / 2, p[1] + h / 2, p[2]] for p in points])
         dst = cv2.perspectiveTransform(points.reshape(-1, 1, 3), projection)
         imgpts = np.int32(dst)
-        if color is False:
-            cv2.fillConvexPoly(img, imgpts, (137, 27, 211))
-        else:
-            color = hex_to_rgb(face[-1])
-            color = color[::-1]  # reverse
-            cv2.fillConvexPoly(img, imgpts, color)
+        cv2.fillConvexPoly(img, imgpts, (137, 27, 211))
 
     return img
 
@@ -84,7 +74,9 @@ if __name__ == "__main__" :
     model_name = glob.glob(model_path+"/*")
     print(model_name)
     assert len(model_name) > 1, "There must be at least 2 visual markers present in the directory."
-    models = [cv2.imread(m,0) for m in model_name[:2]]
+    models = [m for m in model_name[:]]
+    print(models)
+    models = [cv2.imread(m,0) for m in models]
     model = models[0]
 
     print("identifying",model_name[0])
@@ -93,14 +85,15 @@ if __name__ == "__main__" :
 
     while not found:
         ret, frame = capx.read()
-        #cv2.imshow('input',frame)
+        cv2.imshow('input',frame)
+        cv2.waitKey(20)
         #if cv2.waitKey(1) & 0xFF == ord('a'):
             #break
 
         cap = frame
         kp_frame, des_frame = orb.detectAndCompute(cap, None)
 
-        for idx,model in enumerate(models[1:]):
+        for idx,model in enumerate(models):
             kp_model, des_model = orb.detectAndCompute(model, None)
             matches = bf.match(des_model, des_frame)
             print(idx, len(matches))
@@ -110,8 +103,8 @@ if __name__ == "__main__" :
             result = frame
             found = True
 
-    wall = models[0]
-    base = models[1]
+    wall = models[1]
+    base = models[0]
     kp_frame, des_frame = orb.detectAndCompute(cap, None)
     kp_base, des_base = orb.detectAndCompute(base, None)
     kp_wall, des_wall = orb.detectAndCompute(wall, None)
@@ -127,30 +120,45 @@ if __name__ == "__main__" :
     h, w = wall.shape
     pts_wall = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
     dst_wall = cv2.perspectiveTransform(pts_wall, M_wall)
-    print(dst_wall.shape)
+    img2 = cv2.polylines(cap, [np.int32(dst_wall)], True, 255, 3, cv2.LINE_AA)
     proj_mat_wall = projection_matrix(intrinsic_params,M_wall)
 
     h, w = base.shape
     pts_base = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
     dst_base = cv2.perspectiveTransform(pts_base, (M_base))
-    diff = (dst_wall-dst_base)
-    dx = [x[0][0] for x in diff]
-    dy = [x[0][1] for x in diff]
+    img2 = cv2.polylines(cap, [np.int32(dst_wall)], True, 255, 3, cv2.LINE_AA)
 
-    for i in range(N):
-        t = [i*sum(dx)/4*N,i*sum(dy)/4*N]
+    diff = (dst_wall-dst_base)
+    x_base = [x[0][0] for x in dst_base]
+    y_base = [x[0][1] for x in dst_base]
+    x_wall = [x[0][0] for x in dst_wall]
+    y_wall = [x[0][1] for x in dst_wall]
+    cx_base = int(sum(x_base)/len(x_base))
+    cx_wall = int(sum(x_wall)/len(x_wall))
+    cy_base = int(sum(y_base)/len(y_base))
+    cy_wall = int(sum(y_wall)/len(y_wall))
+    print("base",cx_base,cy_base)
+    print("ground",cx_wall,cy_wall)
+    cv2.line(cap,(cx_base,cy_base),(cx_wall,cy_wall),(0,255,0),5)
+    proj_mat_base = projection_matrix(intrinsic_params, (M_base))
+    #finalImg = render(cap, obj, proj_mat_base, model, False)
+
+    for i in range(N+1):
+        framei = cap
+        temp = cap.copy()
+
+        t = [((cx_base-cx_wall)*-1*i)/N, ((cy_base-cy_wall)*-1*i)/N]
         Ht = np.array([[1,0,t[0]],[0,1,t[1]],[0,0,1]]) # translate
-        print(Ht)
         dst_base = cv2.perspectiveTransform(pts_base, Ht.dot(M_base))
         proj_mat_base = projection_matrix(intrinsic_params, Ht.dot(M_base))
-        finalImg = render(cap, obj, proj_mat_base, model, False)
-        cv2.imshow('output',finalImg)
-        cv2.waitKey(100)
+        if i==0:
+           finalImg = cv2.polylines(cap, [np.int32(dst_base)], True, 255, 3, cv2.LINE_AA)
+        finalImg = render(temp, obj, proj_mat_base, model)
+        cv2.imshow('frame', finalImg)
+        cv2.waitKey(10000/N)
 
-
-    #finalImg = render(cap, obj, proj_mat_wall, model, False)
-    proj_mat_base = projection_matrix(intrinsic_params, (M_base))
-    finalImg = render(finalImg, obj, proj_mat_base, model, False)
+        cap = framei
 
     cv2.imshow('output',finalImg)
-    cv2.waitKey(0)
+    cv2.waitKey(1000)
+#    finalImg = render(ffinalImg, obj, proj_mat_base, model, False)
